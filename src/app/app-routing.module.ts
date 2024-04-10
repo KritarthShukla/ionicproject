c3e373bd08d51a0d6d8747d32ab89ecf272e8318
KritarthShukla <kritarthshukla92@gmail.com>
2024-04-10 09:50:14 +0530

    edit-info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,22 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'tab2',
+    loadChildren: () => import('./tab2/tab2.module').then(m => m.Tab2PageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'edit-info',
+    loadChildren: () => import('./edit-info/edit-info.module').then( m => m.EditInfoPageModule)
   }
 ];
 @NgModule({
